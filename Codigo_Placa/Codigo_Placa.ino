d4548bed951efba73f86c955cdfdee584b724562
Francisco Utrera <franciscoutrera@live.com>
2023-09-26 19:42:56 -0300

    Añadido bucle for
    Despues de varios intentos se realizo el delay con un bucle for de 1ms, comprobandose con un osciloscopio que efectivamente hay un delay

--- Codigo_Placa/Codigo_Placa.ino
+++ Codigo_Placa/Codigo_Placa.ino
@@ -1,17 +1,26 @@
 int LED = 13;
 int SensorOptico = 1;
+int PinSalida = 2;
 
 void setup() {
   pinMode(LED, INPUT);
   pinMode(SensorOptico, INPUT);
+  pinMode(PinSalida, OUTPUT);
   digitalWrite(LED, HIGH);
 }
-
+//0,1875 micros / iteracion -> 5.333.333 iteraciones
 void loop() {
-  if(digitalRead(SensorOptico) == HIGH){
-    digitalWrite(LED, LOW);
+  for(int i = 0; i<5300; ++i){
+
   }
-  if(digitalRead(SensorOptico) == LOW){
-    digitalWrite(LED, HIGH);
+  digitalWrite(PinSalida, HIGH);
+  digitalWrite(LED, LOW);
+
+  for(int i = 0; i<5300; ++i){
+    
   }
+
+  digitalWrite(PinSalida, LOW);
+  digitalWrite(LED, HIGH);
+
 }
